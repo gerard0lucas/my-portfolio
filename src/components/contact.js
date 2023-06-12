f9bf8808e551b5778e6ef51ec64c807bfb032e0b
gerard lucas <gerardlucasy2j@gmail.com>
2023-06-13 02:22:12 +0530

    updated

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,7 +10,7 @@ const contact = () => {
         </div>
         <div className="para">
           <p>
-            Although I’m not currently looking for any new opportunities, my
+            I’m currently looking for new opportunities, my
             inbox is always open. Whether you have a question or just want to
             say hi, I’ll try my best to get back to you!
           </p>
